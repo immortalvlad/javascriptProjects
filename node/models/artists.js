9f97db44c5c3d1f008fa6f98dd85cfe3ffb6985a
immortalvlad.g@gmail.com <immortalvlad.g@gmail.com>
2017-05-09 17:25:02 +0300

    commit v17

---
+++ node/models/artists.js
@@ -0,0 +1,37 @@
+var db = require('../db');
+var ObjectId = require('mongodb').ObjectID;
+
+exports.all = function(cb){
+	db.get().collection('artists').find().toArray(function(err, docs){
+		cb(err, docs);
+	});
+};
+
+exports.findById = function(id, cb){
+	db.get().collection("artists").findOne({_id: ObjectId(id)}, function(err, docs){
+		cb(err, docs);
+	});
+};
+
+exports.create = function(artist, cb){
+	db.get().collection('artists').insert(artist, function(err, result){
+		cb(err, result);
+	});
+};
+exports.update = function(id, newData, cb){
+	db.get().collection('artists').updateOne(
+			{_id: ObjectId(id)},
+	newData,
+			function(err, result){
+				cb(err, result)
+			}
+	);
+};
+exports.delete = function(id, cb){
+	db.get().collection('artists').deleteOne(
+			{_id: ObjectId(id)},
+	function(err, result){
+		cb(err, result);
+	}
+	);
+};
--- node/server.js
+++ node/server.js
@@ -5,91 +5,24 @@ var ObjectId = require('mongodb').ObjectID;
 
 var app = express();
 var db = require('./db');
+var artistsConroller = require('./controllers/artists');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
-var artists = [
-	{
-		id: 1,
-		name: 'Metallica'
-	},
-	{
-		id: 2,
-		name: 'Metallica2'
-	},
-	{
-		id: 3,
-		name: 'someother'
-	},
-];
 
 app.get('/', function(req, res){
 	res.send('Hello API');
 });
 
-app.get('/artists', function(req, res){
+app.get('/artists', artistsConroller.all);
 
-	db.get().collection("artists").find().toArray(function(err, docs){
-		if(err){
-			console.log(err);
-			return res.sendStatus(500);
-		}
-		res.send(docs);
-	});
-//	res.send(artists);
-});
+app.get('/artists/:id', artistsConroller.findById);
 
-app.get('/artists/:id', function(req, res){
-	db.get().collection("artists").findOne({_id: ObjectId(req.params.id)}, function(err, docs){
-		if(err){
-			console.log(err);
-			return res.sendStatus(500);
-		}
-		res.send(docs);
-	});
-});
+app.post('/artists', artistsConroller.create);
 
-app.post('/artists', function(req, res){
-	var artist = {
-		name: req.body.name
-	};
-	db.get().collection('artists').insert(artist, function(err, result){
-		if(err){
-			console.log(err);
-			return res.sendStatus(500);
-		}
-		res.send(artist);
-	});
-//	res.send(artist);
-});
+app.put('/artists/:id',artistsConroller.update);
 
-app.put('/artists/:id', function(req, res){
-	db.get().collection('artists').updateOne(
-			{_id: ObjectId(req.params.id)},
-	{name: req.body.name},
-	function(err, result){
-		if(err){
-			console.log(err);
-			res.sendStatus(500);
-		}
-		res.sendStatus(200);
-	}
-	);
-});
-
-app.delete('/artists/:id', function(req, res){
-	db.get().collection('artists').deleteOne(
-			{_id: ObjectId(req.params.id)},
-	function(err, result){
-		if(err){
-			console.log(err);
-			res.sendStatus(500);
-		}
-		res.sendStatus(200);
-	}
-	);
-});
+app.delete('/artists/:id', artistsConroller.delete);
 
 
 db.connect('mongodb://localhost:27017/myapi', function(err){
